270872db53c1393e4e3e8eff71603652dff5bd50
CR7-SW <32232934@qq.com>
2026-09-17 16:48:10 +0800

    Add files via upload

---
+++ data.js.js
@@ -0,0 +1,41 @@
+window.WORKBENCH_DATA = {
+  "version": 1,
+  "theme": "classic",
+  "activeChild": "demo1",
+  "children": [
+    {
+      "id": "demo1",
+      "name": "示例·小军",
+      "theme": "classic",
+      "avatar": "🎖",
+      "avatarImg": "",
+      "avatarSrc": "",
+      "stars": 120,
+      "log": [],
+      "days": {},
+      "streak": 3,
+      "streakBonuses": {},
+      "certificates": [],
+      "cadre": false,
+      "cadrePaidMonths": [],
+      "withdrawals": []
+    },
+    {
+      "id": "demo2",
+      "name": "示例·小旗",
+      "theme": "space",
+      "avatar": "🚩",
+      "avatarImg": "",
+      "avatarSrc": "",
+      "stars": 85,
+      "log": [],
+      "days": {},
+      "streak": 1,
+      "streakBonuses": {},
+      "certificates": [],
+      "cadre": false,
+      "cadrePaidMonths": [],
+      "withdrawals": []
+    }
+  ]
+};
